f82ceedfc64209acc846729b675c6af9eef4e79f
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-11-10 14:45:44 -0300

    configuracao do mariadb

---
+++ src/main/java/br/univille/projetofinal/entity/Usuario.java
@@ -0,0 +1,42 @@
+package br.univille.projetofinal.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String usuario;
+    private String senha;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
